9c010649fbe75a916917235f10db11edb2681f0b
simplyyliam <simplyliam.dev@gmail.com>
2025-08-08 14:40:43 +0200

    feat: added closing and opening functionalities to the create note modal.

--- src/main.js
+++ src/main.js
@@ -1,44 +1,68 @@
 document.addEventListener("DOMContentLoaded", () => {
-const sidebar = document.querySelector('.sidebar');
-const firstBtn = sidebar.querySelector('.nav-btn:first-child');
-const firstBtnIcon = firstBtn.querySelector('svg');
-const firstBtnText = firstBtn.querySelector('h1');
+  const sidebar = document.querySelector(".sidebar");
+  const firstBtn = sidebar.querySelector(".nav-btn:first-child");
+  const firstBtnIcon = firstBtn.querySelector("svg");
+  const firstBtnText = firstBtn.querySelector("h1");
 
-function closeSidebar() {
-  sidebar.style.width = 'max-content';
-  
-  sidebar.querySelectorAll('.nav-btn h1').forEach(h1 => {
-    h1.style.display = 'none';
+  const createModal = document.querySelector(".create-note-modal");
+  const titleInput = createModal.querySelector("#title");
+  const textInput = createModal.querySelector("#text");
+  const actionTools = createModal.querySelector(".action-tools");
+  const closeBtn = createModal.querySelector("#close");
+
+  function closeSidebar() {
+    sidebar.style.width = "max-content";
+
+    sidebar.querySelectorAll(".nav-btn h1").forEach((h1) => {
+      h1.style.display = "none";
+    });
+
+    firstBtn.style.backgroundColor = "transparent";
+    firstBtn.style.borderRadius = "0";
+    firstBtnIcon.style.backgroundColor = "#FFC10750";
+    firstBtnIcon.style.borderRadius = "50%";
+    firstBtnIcon.style.padding = "6px";
+    firstBtnIcon.style.display = "block";
+  }
+
+  function openSidebar() {
+    sidebar.style.width = "280px";
+
+    sidebar.querySelectorAll(".nav-btn h1").forEach((h1) => {
+      h1.style.display = "block";
+    });
+
+    firstBtn.style.backgroundColor = "#FFC10750";
+    firstBtn.style.borderRadius = "0 25px 25px 0";
+
+    firstBtnIcon.style.backgroundColor = "transparent";
+    firstBtnIcon.style.borderRadius = "0";
+    firstBtnIcon.style.padding = "0";
+    firstBtnIcon.style.display = "inline";
+  }
+
+  closeSidebar();
+
+  sidebar.addEventListener("mouseenter", openSidebar);
+  sidebar.addEventListener("mouseleave", closeSidebar);
+
+  function closeModal() {
+    titleInput.style.display = "none";
+    actionTools.style.display = "none";
+  }
+
+  closeModal();
+
+  textInput.addEventListener("focus", () => {
+    titleInput.style.display = "block";
+    actionTools.style.display = "flex";
   });
 
-  firstBtn.style.backgroundColor = 'transparent';
-  firstBtn.style.borderRadius = '0';
-  firstBtnIcon.style.backgroundColor = '#FFC10750'; 
-  firstBtnIcon.style.borderRadius = '50%';
-  firstBtnIcon.style.padding = '6px';
-  firstBtnIcon.style.display = 'block';
-}
+  window.addEventListener('click', () => {
+    closeModal()
+  })
 
-function openSidebar() {
-  sidebar.style.width = '280px';
-
-  sidebar.querySelectorAll('.nav-btn h1').forEach(h1 => {
-    h1.style.display = 'block';
-  });
-
-  firstBtn.style.backgroundColor = '#FFC10750'; 
-  firstBtn.style.borderRadius = '0 25px 25px 0';
-
-  firstBtnIcon.style.backgroundColor = 'transparent';
-  firstBtnIcon.style.borderRadius = '0';
-  firstBtnIcon.style.padding = '0';
-  firstBtnIcon.style.display = 'inline';
-}
-
-closeSidebar();
-
-sidebar.addEventListener('mouseenter', openSidebar);
-sidebar.addEventListener('mouseleave', closeSidebar);
-
-
+  textInput.addEventListener('click', (e) => {
+    e.stopPropagation()
+  })
 });
